6147df3f94ab5c01b8942f5cde2a6e07fe2b7b79
George Imoedemhe <george.imoedemhe@gmail.com>
2018-09-23 13:27:08 +0100

    .

--- main.c
+++ main.c
@@ -14,33 +14,36 @@ main(int argc, char* argv[])
 	list* n_list = ll_new_list(); //create new list
 
 	node nod1 = ll_new_node(smp_txt);
-	ll_add_node(n_list, &nod1);
+	ll_add_node(n_list, &nod1); //add node to list
 
-	node* n = ll_first_node(n_list);
+	node* n = ll_first_node(n_list); //get first node from list
 
 	puts("[--] First node [--]");
 	printf("Node Address: %p\nNode ID: %d\nNode Data: %s\n\n", n, ll_node_id(n), ll_node_data(n));
 
 	node nod2 = ll_new_node(smp_txt2);
 	ll_add_node(n_list, &nod2);
-	n = ll_last_node(n_list); 
+
+	n = ll_last_node(n_list); //get last node from list
 
 	puts("[--] Last node [--]");
 	printf("Node Address: %p\nNode ID: %d\nNode Data: %s\n\n", n, ll_node_id(n), ll_node_data(n));
 
 	//Get IDs of all nodes in the list      
 	ll_all_ids(n_list, ids);
- 	
+
+ 	//iterate through list of node IDs
 	for (size_t i = 0; i < ll_node_count(n_list); ++i) {
 	    printf("ID for %zu   is: %d\n", (i + 1), ids[i]);
 	}
 
-	//Remove second node from list
+	//delete a node from list by node address
 	ll_remove_node_byadr(n_list, &nod2);
 
 	//Get remaining set of node IDs in the list    
 	ll_all_ids(n_list, ids);
  	
+ 	//iterate through list of remaining node IDs
 	for (size_t i = 0; i < ll_node_count(n_list); ++i) {
 	    printf("ID for %zu   is: %d\n", (i + 1), ids[i]);
 	}
